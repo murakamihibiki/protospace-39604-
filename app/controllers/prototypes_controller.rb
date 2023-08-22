a19e256c74fa2603785f7eb132acc3c9899d8767
murakamihibiki <rstoyo.hibiki.murakami@gmail.com>
2023-08-22 16:42:02 +0900

    5

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -19,5 +19,24 @@ class PrototypesController < ApplicationController
     def prototype_params
       params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
     end
-
+    def show
+      '@prototype = Prototype.find(params[:id])
+    end
+    def edit
+      '@prototype = Prototype.find(params[:id])
+    end
+    def update
+      '@prototype = Prototype.find(params[:id])
+      '@prototype.update(prototype_params)
+      if @prototype.save
+        redirect_to root_path
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+    def destroy
+      '@prototype = Prototype.find(params[:id])
+      '@prototype.destroy
+      redirect_to root_path
+    end
 end
