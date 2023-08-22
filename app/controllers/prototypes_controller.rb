32ce19eab82f0487ff42b17a12d680705dd2eebe
murakamihibiki <rstoyo.hibiki.murakami@gmail.com>
2023-08-22 16:01:59 +0900

    2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,23 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = prototype,includes(:user)
   end
+  def new
+    #プロトタイプオブジェクトを生成
+    @prototype = prototype.new
+  end
+  
+  def create
+    @prtotype = current_user.prototypes.new(prototype_params)
+    if @prototype.save
+      redirect_to prototypes_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+    end
+    private
+    def prototype_params
+      params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'prototypes/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
- 
+  devise_for :users
+  resources :prototypes
   root to: 'prototypes#index'
   
 end
