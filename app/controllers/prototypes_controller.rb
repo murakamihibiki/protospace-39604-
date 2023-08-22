a4d653f79feb982836b439cc862e8c7c7fab7e89
murakamihibiki <rstoyo.hibiki.murakami@gmail.com>
2023-08-22 18:35:52 +0900

    8

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,10 +1,10 @@
 class PrototypesController < ApplicationController
   def index
-    @prototypes = prototype,includes(:user)
+    @prototypes = prototype.includes(:user)
   end
   def new
     #プロトタイプオブジェクトを生成
-    @prototype = prototype.new
+    @prototype = Prototype.new
   end
   
   def create
@@ -14,31 +14,32 @@ class PrototypesController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
-    end
+  end
+
     private
-    def prototype_params
-      params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
-    end
-    def show
-      '@prototype = Prototype.find(params[:id])
-      '@comment = Comment.new
-      '@comments = @prototype.comments.includes(:user)
-    end
-    def edit
-      '@prototype = Prototype.find(params[:id])
-    end
-    def update
-      '@prototype = Prototype.find(params[:id])
-      '@prototype.update(prototype_params)
-      if @prototype.save
-        redirect_to root_path
-      else
-        render :edit, status: :unprocessable_entity
-      end
-    end
-    def destroy
-      '@prototype = Prototype.find(params[:id])
-      '@prototype.destroy
-      redirect_to root_path
-    end
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
+  end
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+  def update
+   @prototype = Prototype.find(params[:id])
+   @prototype.update(prototype_params)
+   if @prototype.save
+    redirect_to root_path
+   else
+    render :edit, status: :unprocessable_entity
+   end
+  end
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+  end
 end
