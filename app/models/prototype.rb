1a6e72c303917e7e5a2b52f2e164deeed5ba3d53
murakamihibiki <rstoyo.hibiki.murakami@gmail.com>
2023-08-22 17:07:09 +0900

    6

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -6,5 +6,6 @@ class Prototype < ApplicationRecord
   validates :image, presence: true
   belongs_to :user
   has_one_attached : image
+  has_many :comments
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'prototypes/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
-  resources :prototypes
+  resources :prototypes do
   root to: 'prototypes#index'
   
+end
+root to: 'prototypes#index'
 end
