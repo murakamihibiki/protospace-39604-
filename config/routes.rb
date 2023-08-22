00e28f9800e1fe571ac09dc8609fc9f49d058091
murakamihibiki <rstoyo.hibiki.murakami@gmail.com>
2023-08-22 17:23:38 +0900

    7

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :prototypes do
+  resources :comments, only: :create
   root to: 'prototypes#index'
-  
+
 end
-root to: 'prototypes#index'
+  root to: 'prototypes#index'
+  resources :users, only: :show
 end
